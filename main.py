9b86d7368521c0623aaff5b0ce76f8f439bc04de
sorennx <sorennx@gmail.com>
2022-01-06 01:05:51 +0100

    v0.0.1 - added core classes - Game and Character

--- main.py
+++ main.py
@@ -1,16 +1,94 @@
-# This is a sample Python script.
+import pygame
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#predifened RBG colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+#Images
+BOB_CHARACTER_IMAGE = pygame.image.load(os.path.join('Assets', 'Bob_Character.png'))
+
+TEST_IMAGE = pygame.image.load(os.path.join('Assets','test.png'))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#Characters after scaling
+#BOB_CHARACTER = pygame.transform.scale(BOB_CHARACTER_IMAGE, (150,150))
+
+class Game(object):
+    def __init__(self, gameName, tickRate,width, height):
+        self.gameName = gameName
+        self.tickRate = tickRate
+        self.width = width
+        self.height = height
+        self.gameWindow = self.getGameWindow()
+        self.characterList = []
+        self.characterCount = 0
+        self.createCharacters()
+
+    def getGameWindow(self):
+        window = pygame.display.set_mode((self.width, self.height))  # window
+        pygame.display.set_caption(self.gameName)
+
+        return window
+
+    def createCharacter(self):
+        name = "Bob"
+        healthPoints = 100
+        attackDamage = 10
+        characterImage = BOB_CHARACTER_IMAGE
+        char = Character(name,healthPoints,attackDamage, characterImage)
+        self.characterList.append(char)
+        self.characterCount += 1
+
+    def createCharacters(self, n=2):
+        for i in range(n):
+            self.createCharacter()
+
+    def mainLoop(self):
+        run = True
+        clock = pygame.time.Clock()
+        bob1 = self.characterList[0]
+
+        while run:
+            clock.tick(self.tickRate)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+            self.drawWindow(bob1)
+            self.characterList[0].charPos.x += 1
 
 
-# Press the green button in the gutter to run the script.
+    def drawWindow(self, char1):
+        self.gameWindow.fill(WHITE)
+
+        self.gameWindow.blit(char1.characterImage, (char1.charPos.x, char1.charPos.y))
+        pygame.display.update()
+
+
+class Character(object):
+    def __init__(self, name, healthPoints, attackDamage, characterImage):
+        self.name = name
+        self.hp = healthPoints
+        self.ad = attackDamage
+        self.characterImage = characterImage
+        self.characterWidth = 150
+        self.characterHeight = 150
+        self.xStartCoords = 0
+        self.yStartCoords = 200
+        self.characterVelocity = 5
+        self.charPos = pygame.Rect(self.xStartCoords, self.yStartCoords, self.characterWidth, self.characterHeight)
+
+
+def main():
+    gameName = "gameHobby"
+    WIDTH = 900
+    HEIGHT = 500
+    TICKRATE = 60
+    game = Game(gameName, TICKRATE, WIDTH, HEIGHT)
+    game.mainLoop()
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
